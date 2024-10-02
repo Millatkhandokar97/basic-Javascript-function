689dde31f954b1d50fdee191b23ae77b61a520e1
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-02 20:24:15 +0600

    math function involve in Javascript

---
+++ math.js
@@ -0,0 +1,23 @@
+console.log(Math.E); 
+console.log(Math.PI); 
+
+let num = 9.683
+
+console.log(Math.abs(num)); 
+console.log(Math.floor(num)); 
+console.log(Math.ceil(num)); 
+console.log(Math.round(num));
+console.log(Math.random(num)*100);
+console.log(Math.round(Math.random(num)*100));
+
+
+console.log(Math.max(23, 43, 54, 21, 62, 29, 57));
+console.log(Math.min(23, 43, 54, 21, 62, 29, 57));
+
+
+console.log(Math.pow(2, 7));
+console.log(Math.pow(3, 7));
+
+console.log(Math.sqrt(256));
+console.log(Math.sqrt(64));
+console.log(Math.sqrt(9));
